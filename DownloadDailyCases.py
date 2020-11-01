4dd40be031629213d03a723c61b2338b69e8da58
Jason Mann <aztuxmann+github@gmail.com>
2020-11-01 13:07:12 -0700

    Correct date issue
    Noticed that the date needed to be an int instead of a str.

--- DownloadDailyCases.py
+++ DownloadDailyCases.py
@@ -11,22 +11,22 @@ from pathlib import Path
 #repo_dir = 'AzCovidCasesbyZip'
 #repo = Repo(repo_dir)
 #commit_message = "Upload new xls file"
-date = str(datetime.date.today())
+date = datetime.date.today())
 time_to_get_file = datetime.time(12).strftime("%H") # 24-hour clock req'd, only hour needed
 url = "https://adhsgis.maps.arcgis.com/sharing/rest/content/items/8a2c089c866940bbac0ee70a41ea27bd/data"
 
-#wget.download(url, date+".xls")
-#print("\nGetting file: " + date+".xls")
+#wget.download(url, str(date)+".xls")
+#print("\nGetting file: " + str(date)+".xls")
 downloaded_file = True      # False means that the file hasn't been DL'd today.
 old_date = ""
 old_hour = ""
 
 def GetNewCases():
-    print("\nGetting file: " + date+".xls")
-    wget.download(url, date+".xls")
-    yesterday = date - datetime.timedelta(days=1)
-    file_size = Path(date+".xls").stat().st_size
-    test = filecmp.cmp(date+'.xls', yesterday+'.xls')
+    print("\nGetting file: " + str(date)+".xls")
+    wget.download(url, str(date)+".xls")
+    yesterday = str(date - datetime.timedelta(days=1))
+    file_size = Path(str(date)+".xls").stat().st_size
+    test = filecmp.cmp(str(date)+'.xls', yesterday+'.xls')
     if file_size < 20000:
         print("                              WARNING!!!! File is smaller than expected!\n"*10)
     if test == True:
@@ -36,7 +36,7 @@ def GetNewCases():
 while True:
     current_time = (datetime.datetime.now()).strftime("%H")
     hour = current_time
-    date = str(datetime.date.today())
+    date = datetime.date.today())
 
     if hour != old_hour:
         old_hour = hour
@@ -45,9 +45,9 @@ while True:
     if downloaded_file == False and time_to_get_file == current_time:
         GetNewCases()
         downloaded_file = True
-    elif old_date != date:
+    elif old_date != str(date):
         downloaded_file = False
-        old_date = date
-        print("The date changed to " + date)
+        old_date = str(date)
+        print("The date changed to " + str(date))
     else:
         time.sleep(600)
