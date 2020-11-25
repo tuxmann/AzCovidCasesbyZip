0e4bb990f465015191efde3cb9ab362d2a893a96
Jason Mann <aztuxmann+github@gmail.com>
2020-11-25 14:34:58 -0700

    Updated 2CombineDailyCases.py file
    Creates master cases & daily cases.

--- 2CombineDailyCases.py
+++ 2CombineDailyCases.py
@@ -20,28 +20,61 @@
 # https://realpython.com/pandas-read-write-files/
 # https://xlrd.readthedocs.io/en/latest/api.html
 
-import csv, glob, xlrd
+#import csv, glob, xlrd
+import glob
+import numpy as np
 import pandas as pd
-from xlrd.sheet import ctype_text 
+import matplotlib.pyplot as plt
 
-daily_case_files = sorted((glob.glob('./*.xls')))
+print("Finding files\n")
+daily_case_files = sorted((glob.glob('ADHS_data/*.xls')))
 master_df = pd.DataFrame()
 count = 0
 
 # Get the zipcode:cases & put it in a dictionary and build the DF
+print("Creating Master Cases DataFrame.\n")
 for file in daily_case_files:
 	fname = (((str(file)).split('/')[1]).split('.')[0]) # For LINUX
-	fname = (((str(file)).split('\\')[1]).split('.')[0]) # For WINDOWS
+	#fname = (((str(file)).split('\\')[1]).split('.')[0]) # For WINDOWS
 	df = pd.read_excel(file)
 	ZipCaseDict = dict(zip(df.POSTCODE, df.ConfirmedCaseCount))
 	if count == 0:
 		master_df = pd.DataFrame(zip(df.POSTCODE, df.ConfirmedCaseCount))
 		master_df = master_df.rename(columns={0:'ZipCode',1:fname})
-		print(master_df)
 		count += 1
 	else:
 		master_df[fname] = master_df['ZipCode'].map(ZipCaseDict)
 		count += 1
 
-print(master_df)
+# Remove the word Tribal from zipcodes. Tribal areas are currently
+# supressing coronavirus data, so we're blind to these areas.
+zip = master_df['ZipCode']
+tribal = zip.str.contains('Tribal')
+master_df['ZipCode'] = np.where(tribal, zip.str.replace(' Tribal',''), master_df['ZipCode'])
+
+# Replacing supressed data with -100 for future color option.
+# Replacing 1-10 value with 10 because we should assume the worst.
+master_df = master_df.replace(['Data Suppressed'],'-100')
+master_df = master_df.replace(['1-10'],'10')
+
+print("Saving Master DataFrame to disk.\n")
+master_df = master_df.set_index('ZipCode')
 master_df.to_csv('master_cases.csv')
+
+print("Creating New Cases by Day DataFrame")
+# Change all the cell values to INT and calculate the new cases
+master_df = master_df.astype(int)
+daily_cases_df = master_df.diff(axis=1)
+daily_cases_df.to_csv('daily_cases.csv')
+
+print("Program finished. Look for master_cases.csv in this folder.")
+
+
+
+
+#daily_cases_df = daily_cases_df.T
+#print(master_df)
+#print(daily_cases_df.sum())
+
+#daily_cases_df.plot()
+#plt.show()
